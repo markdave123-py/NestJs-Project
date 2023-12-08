25dfff0cdfa4ba5e50e25dbfe2ede48f6029d3a7
markdave123-py <nwaekwudavid@gmail.com>
2023-12-08 13:12:16 +0100

    changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { CustomersModule } from './customers/customers.module';
-import { UsersModule } from './users/users.module';
+
 
 
 @Module({
-  imports: [CustomersModule, UsersModule],
+  imports: [],
   controllers: [],
   providers: [],
 })
